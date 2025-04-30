5716e980ecec870596a209adbddc91551954f0a7
Shaurya Singh <109419898+ProCoder5534@users.noreply.github.com>
2025-04-30 15:02:54 +0530

    fixed flipping word and made responsive

--- src/App.jsx
+++ src/App.jsx
@@ -21,32 +21,37 @@ function App() {
       <Header />
       
       <AuroraBackground>
-        <motion.div
-          initial={{ opacity: 0.0, y: 40 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{
-            delay: 0.3,
-            duration: 0.8,
-            ease: "easeInOut",
-          }}
-          className="relative flex flex-col gap-4 items-center justify-center px-4"
-        >
-          <div className="text-3xl md:text-7xl font-bold dark:text-white text-center" >
-            Parliamentary Debate Championship 2025
-          </div>
-          <div className=" font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
-            Join the ultimate clash of minds! <span><FlipWords words={["Compete.", "Debate.", "Shine."]} duration={2000} className=" text-4xl font-semibold gangnam-style"/></span> 
-          </div>
-          
-          <button
-            href="#register"
-            className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2"
-          >
-            <a href="#register"> Register now</a>
-          </button>
-        </motion.div>
-      </AuroraBackground>
-      
+  <motion.div
+    initial={{ opacity: 0.0, y: 40 }}
+    whileInView={{ opacity: 1, y: 0 }}
+    transition={{
+      delay: 0.3,
+      duration: 0.8,
+      ease: "easeInOut",
+    }}
+    className="relative flex flex-col gap-4 items-center justify-center px-4"
+  >
+    <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
+      Parliamentary Debate Championship 2025
+    </div>
+    <div className="font-extralight text-xl sm:text-3xl md:text-4xl leading-normal sm:leading-relaxed dark:text-neutral-200 py-4">
+      Join the ultimate clash of minds!{" "}
+      <span className="inline-block w-[100px] sm:w-[110px] md:w-[120px] text-center">
+        <FlipWords
+          words={["Compete.", "Debate.", "Shine."]}
+          duration={2000}
+          className="text-xl sm:text-3xl md:text-4xl font-semibold gangnam-style"
+        />
+      </span>
+    </div>
+
+    <button
+      className="bg-black dark:bg-white rounded-full w-fit text-sm sm:text-base text-white dark:text-black px-4 py-2"
+    >
+      <a href="#register">Register now</a>
+    </button>
+  </motion.div>
+</AuroraBackground>
       <main>
         {/* <Hero /> */}
       
